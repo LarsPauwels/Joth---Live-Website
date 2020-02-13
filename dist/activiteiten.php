5d12e63bf3856d24cb62b712bc9e4a8174945021
Irene Haegeman <r0702465@student.thomasmore.be>
2020-02-13 15:09:06 +0100

    Add pages

---
+++ dist/activiteiten.php
@@ -0,0 +1,1 @@
+<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Document</title></head><body> <?php require_once 'navigation.php'; ?> <h1>Activiteiten</h1><ul><li></li><li></li></ul></body></html>
---
+++ src/vragen.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>JOTH - vragen</title>
+</head>
+<body>
+	<?php require_once 'navigation.php'; ?>
+	<h1>Veelgestelde vragen</h1>
+	<ul>
+		<li>
+			<h2>Wanneer kan ik terecht in het JOTH-huis?</h2>
+			<p></p>
+		</li>
+	</ul>
+</body>
+</html>
