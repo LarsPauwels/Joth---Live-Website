e613291f47e5427fb7f4b1f23eaef305e199fa6e
Irene Haegeman <r0702465@student.thomasmore.be>
2020-02-15 23:08:58 +0100

    [FOOTER] Optimize footer

--- src/footer.php
+++ src/footer.php
@@ -1,27 +1,37 @@
 <footer class="footer__container">
 	<div class="footer__bg">
 		<svg class="footer__bg--svg">
-			<circle class="footer__bg--circle" cx="6.5%" cy="15.5%" r="40"   alignment-baseline="center"/>
+			<circle class="footer__bg--circle" cx="5.1%" cy="11.5%" r="40"   alignment-baseline="center"/>
 		</svg>
 	</div>
 	<div class="footer__content">
 		<h2 class="footer__title">JOTH-huis</h2>
 		<ul class="footer__items">
 			<li class="footer__item footer__item--left">
-				<p>info@joth.be</p>
-				<p>Adres</p>
+				<p class="footer__item--contact">info@joth.be</p>
+				<p class="footer__item--title">Adres</p>
 				<p>Vierselbaan 13</p>
 				<p>2240 zandhoven</p>
 			</li>
 			<li class="footer__item footer__item--right">
-				<p>03 336 38 54</p>
-				<p>Openingsuren</p>
+				<p class="footer__item--contact">03 336 38 54</p>
+				<p class="footer__item--title">Openingsuren</p>
 				<p>Woensdag 12u - 17u</p>
 				<p>Vrijdag 16u - 21u</p>
 				<p>Zaterdag 9u - 12u</p>
 			</li>
 		</ul>
-		<img src="/assets/fb.svg" alt="Facebook icoon">
-		<img src="/assets/instagram.svg" alt="Instagram icoon">
+		<ul class="footer__socials">
+			<li class="footer__socials--fb">
+				<a href="https://www.facebook.com/joth.zandhoven/?ref=py_c">
+					<img src="assets/fb.svg" alt="Facebook icoon">
+				</a>
+			</li>
+			<li class="footer__socials--instagram">
+				<a href="https://www.instagram.com/joth_zandhoven/">
+					<img src="assets/instagram.svg" alt="Instagram icoon">
+				</a>
+			</li>
+		</ul>
 	</div>
 </footer>
