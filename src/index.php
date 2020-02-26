8e9d6460fb0b7d5b6c713a6f071d7a0dadc15a06
Irene Haegeman <r0702465@student.thomasmore.be>
2020-02-27 00:09:38 +0100

    [HTML/SASS] Fix footer

--- src/index.php
+++ src/index.php
@@ -14,28 +14,28 @@
     </main>
     <section class="home__about">
       <ul>
-        <li>
+        <li class="section__item">
           <h2>Over joth</h2>
         </li>
-        <li>
-          <p>
+        <li class="section__item">
+          <p class="section__item__paragraph">
             In het JOTH-huis kan je als jongere van 12 tot en met 25 jaar gewoon binnen en buiten lopen en allerlei leuke activiteiten doen. Het is een veilige plek waar je ook een luisterend oor vindt en beroep kan doen op professionele therapeutische hulp, gratis en zonder een label opgeplakt te krijgen.
           </p>
-          <button class="btn btn__light">Ontmoet de vrijwilligers</button>
+          <button class="btn btn__section btn__light">Ontmoet de vrijwilligers</button>
         </li>
       </ul>
     </section>
     <section class="home__contact">
       <ul class="contact__list">
-        <li>
+        <li class="section__item">
           <h2>Met open armen</h2>
         </li>
-        <li>
-          <p>
+        <li class="section__item">
+          <p class="section__item__paragraph">
             Even ontsnappen aan de drukte thuis? De jongerenmedewerkers van JOTH staan voor je klaar. Kom langs in ons JOTH-huis in Zandhoven en ontspan je even in onze gezellige zithoek, aan de kickertafel of aan de piano. 
             Eerder nood aan een babbel? Maak al online een afspraak voor een een-op-eengesprek.
           </p>
-          <button class="btn btn__light">Maak een afspraak</button>
+          <button class="btn btn__section btn__light">Maak een afspraak</button>
         </li>
       </ul>
     </section>
